b96ef7a9d0dd7abba521bbece8f286682521cf25
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-23 13:25:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/favicon.ico" type="img/ico">
+    <title>PHP OOP 1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
